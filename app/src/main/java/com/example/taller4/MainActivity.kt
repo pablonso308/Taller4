8c9fb01138b5e387492b2e589d3fad480830f881
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-11-21 11:23:33 +0100

    definicion de la pantalla de inicio

--- app/src/main/java/com/example/taller4/MainActivity.kt
+++ app/src/main/java/com/example/taller4/MainActivity.kt
@@ -1,29 +1,27 @@
 package com.example.taller4
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.taller4.ui.theme.Taller4Theme
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Taller4Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MainScreen {
+                    startActivity(Intent(this, MainActivity::class.java))
                 }
             }
         }
@@ -31,17 +29,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(onNavigateToMain: () -> Unit) {
+    val greeting = getGreetingMessage()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = greeting)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onNavigateToMain) {
+            Text(text = "Go to Main Screen")
+        }
+    }
+}
+
+fun getGreetingMessage(): String {
+    val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+    return when (currentHour) {
+        in 0..11 -> "Buenos días"
+        in 12..17 -> "Buenas tardes"
+        else -> "Buenas noches"
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     Taller4Theme {
-        Greeting("Android")
+        MainScreen {}
     }
 }
