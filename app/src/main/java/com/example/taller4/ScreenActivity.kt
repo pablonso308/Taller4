cfdefb9a799e9d624aff3b63509b8d737242ae7a
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-11-21 20:54:15 +0100

    modificacion de ScreenActivity y de la xml

--- app/src/main/java/com/example/taller4/ScreenActivity.kt
+++ app/src/main/java/com/example/taller4/ScreenActivity.kt
@@ -1,12 +1,19 @@
-// SecondActivity.kt
 package com.example.taller4
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.taller4.fragments.DetailFragment
+import com.example.taller4.fragments.ListFragment
 
-class ScreenActivity : AppCompatActivity() {
+class ScreenActivity : AppCompatActivity(), ListFragment.OnItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen)
+    }
+
+    override fun onItemSelected(item: String) {
+        val detailFragment = supportFragmentManager.findFragmentById(R.id.detailFragment) as DetailFragment
+        detailFragment.updateDetail(item)
     }
 }
